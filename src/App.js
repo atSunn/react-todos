c63c4f204f30a4d7950e4ced3c98212199a31004
Athira K Sunny <athirabeenasunny@gmail.com>
2020-05-22 14:55:19 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> To Do List</div >
+    );
+  }
+}
+
+export default App;
